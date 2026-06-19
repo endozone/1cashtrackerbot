9faaf9eef611d84dbfcdda6ef27913223bc95a99
endozone <kuvernuit@gmail.com>
2026-06-18 21:51:33 -0400

    Update post_tweet.py

--- post_tweet.py
+++ post_tweet.py
@@ -2,12 +2,20 @@ import os
 import tweepy
 from gradio_client import Client
 
-# 1. ask your Space for one tweet
+# 1. generate a tweet from your Space
 space = Client("iloveworldpeace/tweetbot", token=os.environ["HF_TOKEN"])
 tweet_text = space.predict(0.9, 1, api_name="/generate")
-tweet_text = tweet_text.strip()[:280]                    # stay under X's limit
+tweet_text = tweet_text.strip()[:280]
 
-# 2. post it
+# 2. set up the X client
+client = tweepy.Client(
+    consumer_key=os.environ["X_API_KEY"],
+    consumer_secret=os.environ["X_API_SECRET"],
+    access_token=os.environ["X_ACCESS_TOKEN"],
+    access_token_secret=os.environ["X_ACCESS_SECRET"],
+)
+
+# 3. diagnostic: check credentials, then try to post
 try:
     me = client.get_me()
     print("Credentials OK. Logged in as:", me.data.username)
