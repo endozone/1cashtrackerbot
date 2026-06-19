4f2dcdb56ebbd54a7905a7c849d548e181474651
endozone <kuvernuit@gmail.com>
2026-06-18 20:55:39 -0400

    Add script to post tweets using Tweepy and Gradio
    This script fetches a tweet from a Gradio space and posts it to Twitter using the Tweepy library.

---
+++ post_tweet.py
@@ -0,0 +1,18 @@
+import os
+import tweepy
+from gradio_client import Client
+
+# 1. ask your Space for one tweet
+space = Client("iloveworldpeace/tweetbot")
+tweet_text = space.predict(0.9, 1, api_name="/predict")  # temperature 0.9, 1 tweet
+tweet_text = tweet_text.strip()[:280]                    # stay under X's limit
+
+# 2. post it
+client = tweepy.Client(
+    consumer_key=os.environ["X_API_KEY"],
+    consumer_secret=os.environ["X_API_SECRET"],
+    access_token=os.environ["X_ACCESS_TOKEN"],
+    access_token_secret=os.environ["X_ACCESS_SECRET"],
+)
+client.create_tweet(text=tweet_text)
+print("Posted:", tweet_text)
